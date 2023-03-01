94c0de34eeff85cc1b31c9fcd7b640a49f821e12
blackberry12562 <116704068+blackberry12562@users.noreply.github.com>
2023-03-01 19:46:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,15 +4,15 @@
 
 
 double pown(double value, uint16_t n) {
-  double sum = 1.0;
-  for (int i = 0; i < n; i++) {
-    sum *= value;
+  if (0 == n) {
+    return 1;
+  } else {
+    return value * pown(value, n - 1);
   }
-  return sum;
 }
 
 uint64_t fact(uint16_t n) {
-  if (n == 1) {
+  if (0 == n) {
     return 1;
   } else {
     return n * fact(n - 1);
@@ -20,29 +20,31 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-  return pown(x, n) / fact(n);
+  return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
   double sum = 1.0;
   for (int i = 1; i <= count; i++) {
     sum += calcItem(x, i);
-  } 
+  }
   return sum;
 }
 
 double sinn(double x, uint16_t count) {
   double sum = 0.0;
-  for (int i = 0; i < count; i++) {
-    res += calcItem(x, 2*i+1) * pown(-1, i);
+  while (count > 0) {
+    sum += pown(-1, count - 1) * calcItem(x, 2 * count - 1);
+    --count;
   }
   return sum;
 }
 
 double cosn(double x, uint16_t count) {
   double sum = 0.0;
-  for (int i = 0; i < count; i++) {
-    res += calcItem(x, 2*i) * pown(-1, i);
+  while (count > 0) {
+    sum += pown(-1, count - 1) * calcItem(x, 2 * count - 2);
+    --count;
   }
   return sum;
 }
