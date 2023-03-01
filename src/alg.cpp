e6b9ea8456f34b115bb1c252cfc7c4300ad059ad
blackberry12562 <116704068+blackberry12562@users.noreply.github.com>
2023-03-01 19:33:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,21 +13,39 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-
+  if (n == 1) {
+    return 1;
+  } else {
+    return n * fact(n - 1);
+  }
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-
+  double sum = 1.0;
+  for (int i = 1; i <= count; i++) {
+    sum += calcItem(x, i);
+  } 
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-
+  double sum = 0.0;
+  while (count > 0) {
+    sum += pown(-1, count - 1) * calcItem(x, 2 * count - 1);
+    --count;
+  }
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-
+  double sum = 0.0;
+  while (count > 0) {
+    sum += pown(-1, count - 1) * calcItem(x, 2 * count - 2);
+    --count;
+  }
+  return sum;
 }
